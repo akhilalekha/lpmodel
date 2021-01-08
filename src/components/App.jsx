54a9a89b3d6e10fe407a9a1d7b5bd6cbc923935d
akhilalekha <akhila160@gmail.com>
2021-01-08 21:27:41 +0530

    add images section and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./Home";
+import Images from "./Images";
 
 function App() {
 	return (
 		<div className="App">
 			<Home />
+			<Images />
 		</div>
 	);
 }
---
+++ src/components/Images.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Images.css";
+
+function Images() {
+	return (
+		<div className="Images">
+			<h1 className="imgTitle">Quae quo sunt excelsiores Dant clariora</h1>
+			<div className="imagesBox">
+				<div className="image img1">
+					<div className="imageOverlay"></div>
+					<p className="description">Quae quo sunt excelsiores Dant clariora</p>
+				</div>
+				<div className="image img2">
+					<div className="imageOverlay"></div>
+					<p className="description">Quae quo sunt excelsiores Dant clariora</p>
+				</div>
+				<div className="image img3">
+					<div className="imageOverlay"></div>
+					<p className="description">Quae quo sunt excelsiores Dant clariora</p>
+				</div>
+				<div className="image img4">
+					<div className="imageOverlay"></div>
+					<p className="description">Quae quo sunt excelsiores Dant clariora</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Images;
