1c180868962b3d0d0a66782bec104a664f1e24ae
akhilalekha <akhila160@gmail.com>
2021-01-12 02:03:27 +0530

    fix key prop in ftbox

--- src/components/FtBox.jsx
+++ src/components/FtBox.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import style from "./FtBox.module.css";
+import { v4 as uuidv4 } from "uuid";
 function FtBox({ title, links }) {
 	return (
 		<div className={style.FtBox}>
 			<p className={style.FtBoxTitle}>{title}</p>
 			{links.map((link) => (
-				<p className={style.FtBoxLink}>{link}</p>
+				<p className={style.FtBoxLink} key={uuidv4()}>
+					{link}
+				</p>
 			))}
 		</div>
 	);
